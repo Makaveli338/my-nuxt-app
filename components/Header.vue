604e922cd9024cb151e31487df28a116b5c258d0
John Kimotho <johnkimotho75@gmail.com>
2025-02-03 15:03:57 +0300

    init commit

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto flex items-center justify-between p-4">
+        <img src="/img/logo.svg" class="h-[61px] w-[270px]" />
+
+        <div class="flex items-center justify-end">
+            <button class="bg-[white] hover:blue-700 text-black font-bold border-[1px] border-[#D0D5DD] rounded-[50px] cursor-pointer 
+            items-center w-[120px] h-11 mt-[10px] text-center">OTP Log in</button>
+            <button class="bg-[#0047BA] hover:blue-100 text-[white] font-bold rounded-[50px] cursor-pointer 
+            items-center w-[152px] h-11 mt-[10px]  text-center ml-3">eCitizen Log in</button>
+        </div>
+    </div>
+</template>
